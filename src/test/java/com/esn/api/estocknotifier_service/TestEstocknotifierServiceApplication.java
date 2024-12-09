30e14d279da02778ff884df14526a35efa4c1c78
Indrajit <indrajitbastapure@gmail.com.com>
2024-12-09 18:26:56 +0100

    Change mail application class name

--- src/test/java/com/esn/api/estocknotifier_service/TestEstocknotifierServiceApplication.java
+++ src/test/java/com/esn/api/estocknotifier_service/TestEstocknotifierServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestEstocknotifierServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(EstocknotifierServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
